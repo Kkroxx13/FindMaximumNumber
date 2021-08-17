f8fb8057d0625996c72123a58c12bf0fa215a2bc
Karan kanchan <kanchan.karan.17ce7002@gmail.com>
2021-08-17 22:19:11 +0530

    Program to find maximum number

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FindMaximumNumberProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int output = MaximumNumberCheck.MaximumIntegerNumber(11, 22, 33);
+            Console.WriteLine(output);
+        }
+    }
+}
